48a6b85cec77da0da4a3d990c74c62aeca6dd851
Chiisom <theodore_mca@hordanso.com>
2022-03-14 01:56:15 +0100

    added portfolio link to resume

--- src/views/indexComponents/Resume.vue
+++ src/views/indexComponents/Resume.vue
@@ -49,7 +49,8 @@
           </div>
         </div>
       </div>
-      <p class="text-center mt-5 wow fadeInUp" style="visibility: visible; animation-name: none;"><a href="#" 
+      <p class="text-center mt-5 wow fadeInUp" style="visibility: visible; animation-name: none;">
+        <a href="https://chiisom03.github.io/peter__resume.pdf" target="blank"
         class="btn btn-outline-dark shadow-none rounded-0">Download CV</a>
         </p>
     </div>
