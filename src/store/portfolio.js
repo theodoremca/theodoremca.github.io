a33f5001b2f0e1d6add343f0ba2a2d2c7e056216
Cynthia Obasuyi <cynthiaobasuyi68@gmail.com>
2022-04-25 03:28:52 +0100

    added more infor

--- src/store/portfolio.js
+++ src/store/portfolio.js
@@ -1,27 +1,7 @@
 export default {
   state: {
     data: [
-      {
-        name: "Calendar",
-        image: "images/calendar.jpg",
-        category: "UI Design",
-        link: "/",
-        tag: "ui",
-      },
-      {
-        name: "Mobile App",
-        image: "images/flutter_ad.jpg",
-        category: "Mobile App",
-        link: "ajax/portfolio-ajax-project-2.html",
-        tag: "flutter-dev",
-      },
-      {
-        name: "Laraval 'n' Vue",
-        image: "images/LaravelVue.jpg",
-        category: "Websites",
-        link: "ajax/portfolio-ajax-project-3.html",
-        tag: "web-dev",
-      },
+
       {
         name: "XCode Tech UI Design",
         video: "https://youtube.com/embed/fyc7wdOdqro",
@@ -37,44 +17,9 @@ export default {
         tag: "ui",
       },
       {
-        name: "Flutter",
-        image: "images/flutter_ad2.jpg",
-        category: "Category",
-        link: "ajax/portfolio-ajax-project-2.html",
-        tag: "flutter-dev",
-      },
-      {
-        name: "Web dev",
-        image: "images/project-4.jpg",
-        category: "Web dev",
-        link: "ajax/portfolio-ajax-project-3.html",
-        tag: "web-dev",
-      },
-      {
         name: "Web Calculator",
         video: "https://youtube.com/embed/GKu1Iq9XEmI",
         category: "Category",
-        tag: "web-dev",
-      },
-      {
-        name: "UI Design",
-        image: "images/project-2.jpg",
-        category: "Category",
-        link: "ajax/portfolio-ajax-project-1.html",
-        tag: "ui",
-      },
-      {
-        name: "Flutter dev",
-        image: "images/project-3.jpg",
-        category: "Category",
-        link: "ajax/portfolio-ajax-project-2.html",
-        tag: "flutter-dev",
-      },
-      {
-        name: "Web dev",
-        image: "images/project-4.jpg",
-        category: "Category",
-        link: "ajax/portfolio-ajax-project-3.html",
         tag: "web-dev",
       },
       {
